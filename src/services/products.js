43908ade9494a900bab9dc96c1d57c807bdc9453
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-11-01 22:48:33 +0200

    task4

--- src/services/products.js
+++ src/services/products.js
@@ -1,3 +1,15 @@
 import { ProductCollection } from '../db/models/product.js';
 
 export const getProducts = () => ProductCollection.find();
+
+// export const getProductById = async (id) => {
+//   console.log(id);
+
+//   return await ProductCollection.find({ _id: id });
+// };
+
+export const getProductById = async (id) => {
+  console.log(id);
+
+  return await ProductCollection.findById(id);
+};
